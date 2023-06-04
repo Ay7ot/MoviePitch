73459ac08404426770ec77770e45f46f00b82146
Aydot <ayomidotzee@gmail.com>
2023-06-04 01:01:50 -0700

    API changed

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
 
     return (
-        <footer className="fixed bottom-0 flex py-4 bg-[#242526] gap-32 w-full items-center justify-center rounded-t-lg">
+        <footer className="fixed bottom-0 flex py-2 bg-[#242526] gap-32 w-full items-center justify-center rounded-t-lg">
             <p className="text-white">&copy; Copyright Spandor 2023</p>
         </footer>
     )
--- src/Components/Setup.tsx
+++ src/Components/Setup.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState  } from "react"
 import { useAuth } from "../useAuth"
 import { fetchImage, fetchText } from "../openai"
 
